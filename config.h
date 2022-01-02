f9de21822e9e0244c921e0ddadfd3cf1298ce77d
mkinz <matthew.kinzler@gmail.com>
2022-01-02 17:03:38 -0500

    add hotkey for launching browser

--- config.h
+++ config.h
@@ -117,6 +117,7 @@ static Key keys[] = {
 	{ MODKEY,			            XK_equal,  setgaps,	   {.i = +1 } },
 	{ MODKEY|ShiftMask,		        XK_equal,  setgaps,	   {.i =  0 } },
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
+	{ MODKEY|ControlMask,	        XK_b,      spawn,	       SHCMD("firefox") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
